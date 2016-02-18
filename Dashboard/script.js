5b69bc66b979281f0181bf64546b822a58527981
Fagot <RFA3456@viseo.net>
2016-02-18 14:41:31 +0100

    update

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -25,7 +25,8 @@ var stats = {
 	interviewed: 0,
 	hired: 0,
 	rejected: 0,
-	notinterested: 0
+	notinterested: 0,
+	hasResponse: 0
 };
 
 var lists = {
@@ -34,154 +35,107 @@ var lists = {
 	interviewed: "56b8a4f7bd73d563ef52a309",
 	hired: "56b8a4adc74b5f465aad5602",
 	rejected: "56bb38e2c575ea2f58e23024",
-	notinterested: "56bb39b91d56ccd8a899a2e0"
+	notinterested: "56bb39b91d56ccd8a899a2e0",
+	hasResponse: "56c49587b04c088cf872a11d"
 };
 
-var dataTable = [];
+var labels = {
+	contacted: "56b8a420152c3f92fde3ec07",
+	hasResponse: "56b8a420152c3f92fde3ec05",
+	notinterested: "56b8a420152c3f92fde3ec06",
+	rejected: "56b8a420152c3f92fde3ec08",
+	called: "56b8a420152c3f92fde3ec09",
+	hired: "56b8a420152c3f92fde3ec0a",
+	interviewed: "56c495c1152c3f92fd100d54"
+};
 
+
+var countLabels = function(cardTab) {
+	var labelIds = Object.keys(labels).map(function(key){return labels[key]});
+
+
+	for(i = 0 ; i < cardTab.length ; i++) {
+		var cardLabels = cardTab[i].labels;
+
+		for(l = 0 ; l < cardLabels.length ; l++) {
+			var labelIndex = $.inArray(cardLabels[l].id, labelIds);
+			if (labelIndex != -1) {
+				var category = Object.keys(labels)[labelIndex];
+				stats[category]++;// = stats[Object.keys(labels)[labelIndex]] + 1;
+			}
+		}
+	}
+}
 
 var fail = function() {console.log("fail");}
-var displayContacted = function(data) {
-	$("#contacted").text( data.length);
-	stats.contacted = data.length;
-	dataTable.push(['contacted', data.length]);
-}
 
-var displayCalled = function(data) {
-	$("#called").text( data.length);
-	stats.called = data.length;
-	dataTable.push(['called', data.length]);
-}
-
-var displayInterviewed = function(data) {
-	$("#interviewed").text( data.length);
-	stats.interviewed = data.length;
-	dataTable.push(['interviewed', data.length]);
-}
-
-var displayHired = function(data) {
-	$("#hired").text( data.length);
-	stats.hired = data.length;
-	dataTable.push(['hired', data.length]);
-}
-
-
-var displayRejected = function(data) {
-	$("#rejected").text( data.length);
-	stats.rejected = data.length;
-	dataTable.push(['rejected', data.length]);
-}
-
-var displayNotInterested = function(data) {
-	$("#notinterested").text( data.length);
-	stats.notinterested = data.length;
-	dataTable.push(['not interrested', data.length]);
-}
-
-var getStats = function () {
-	// Trello.get('/lists/'+lists.contacted+'/cards', displayContacted, fail);
-	// Trello.get('/lists/'+lists.called+'/cards', displayCalled, fail);
-	// Trello.get('/lists/'+lists.interviewed+'/cards', displayInterviewed, fail);
-	// Trello.get('/lists/'+lists.hired+'/cards', displayHired, fail);
-	// Trello.get('/lists/'+lists.rejected+'/cards', displayRejected, fail);
-	Trello.get('/lists/'+lists.notinterested+'/cards', displayNotInterested, fail);
-}
-
-var getContacted = function(cardID) {
-
+var getCardsFromList = function(cardID) {
 	var deferred = $.ajax({
 		url: "https://api.trello.com/1/lists/"+cardID+"/cards?key=b1f2a0219e4c69b472b5721208b95535&token=4c0ab28b6f92a1f13abf9fc1e1ac3932345135a62ea248897e6f33c7c2a472c1",
        
     }).then(function(response){ 
-       displayContacted(response);
+       countLabels(response);
     });
     return deferred;
 }
 
-var getCalled = function(cardID) {
-
-	var deferred = $.ajax({
-		url: "https://api.trello.com/1/lists/"+cardID+"/cards?key=b1f2a0219e4c69b472b5721208b95535&token=4c0ab28b6f92a1f13abf9fc1e1ac3932345135a62ea248897e6f33c7c2a472c1",
-       
-    }).then(function(response){ 
-       displayCalled(response);
-    });
-    return deferred;
-}
-
-var getInterviewed = function(cardID) {
-
-	var deferred = $.ajax({
-		url: "https://api.trello.com/1/lists/"+cardID+"/cards?key=b1f2a0219e4c69b472b5721208b95535&token=4c0ab28b6f92a1f13abf9fc1e1ac3932345135a62ea248897e6f33c7c2a472c1",
-       
-    }).then(function(response){ 
-       displayInterviewed(response);
-    });
-    return deferred;
-}
-
-var getHired = function(cardID) {
-
-	var deferred = $.ajax({
-		url: "https://api.trello.com/1/lists/"+cardID+"/cards?key=b1f2a0219e4c69b472b5721208b95535&token=4c0ab28b6f92a1f13abf9fc1e1ac3932345135a62ea248897e6f33c7c2a472c1",
-       
-    }).then(function(response){ 
-       displayHired(response);
-    });
-    return deferred;
-}
-
-var getRejected = function(cardID) {
-
-	var deferred = $.ajax({
-		url: "https://api.trello.com/1/lists/"+cardID+"/cards?key=b1f2a0219e4c69b472b5721208b95535&token=4c0ab28b6f92a1f13abf9fc1e1ac3932345135a62ea248897e6f33c7c2a472c1",
-       
-    }).then(function(response){ 
-       displayRejected(response);
-    });
-    return deferred;
-}
-
-var getNotInterested = function(cardID) {
-
-	var deferred = $.ajax({
-		url: "https://api.trello.com/1/lists/"+cardID+"/cards?key=b1f2a0219e4c69b472b5721208b95535&token=4c0ab28b6f92a1f13abf9fc1e1ac3932345135a62ea248897e6f33c7c2a472c1",
-       
-    }).then(function(response){ 
-       displayNotInterested(response);
-    });
-    return deferred;
+var displayStats = function() {
+	$("#contacted").text( stats.contacted);
+	$("#hasResponse").text( stats.hasResponse);
+	$("#called").text( stats.called);
+	$("#interviewed").text( stats.interviewed);
+	$("#hired").text( stats.hired);
+	$("#rejected").text( stats.rejected);
+	$("#notinterested").text( stats.notinterested);
+	
 }
 
 var displayChart = function() {
-	var data = new google.visualization.DataTable();
-	data.addColumn('string', 'Type');
-	data.addColumn('number', 'Quantité');
-	data.addRows(dataTable);
-	var chart = new google.visualization.PieChart(document.getElementById('chart'));
-    chart.draw(data, null);
+	var data = google.visualization.arrayToDataTable([
+        ["Type", "Pourcentage", { role: "style" } ],
+		["Embauches", stats.hired*1.0/stats.contacted*100, "color: #e5e4e2"],
+        ["Non retenus", stats.rejected*1.0/stats.contacted*100, "color: #e5e4e2"],
+        ["Non intéressés", stats.notinterested*1.0/stats.contacted*100, "color: #e5e4e2"],
+        ["Entretiens présentiels", stats.interviewed*1.0/stats.contacted*100, "color: #e5e4e2"],
+        ["Entretiens téléphoniques", stats.called*1.0/stats.contacted*100, "gold"],
+        ["Réponses obtenues", stats.hasResponse*1.0/stats.contacted*100, "silver"],
+        ["Contactés", stats.contacted*1.0/stats.contacted*100, "#b87333"]
+      ]);
+
+	var options = {
+          chart: {
+            title: 'Statistiques recrutement',
+            subtitle: 'Résultats sur Viadéo',
+          },
+          bars: 'horizontal' // Required for Material Bar Charts.
+        };
+
+	var chart = new google.charts.Bar(document.getElementById("chart"));
+    chart.draw(data, options);
+}
+
+var computeStats = function() {
+	//stats.contacted = stats.called + stats.hasResponse + stats.interviewed + stats.hired + stats.rejected + stats.notinterested ;
+}
+
+var refreshPage = function() {
+	computeStats();
+	displayStats();
+	displayChart();
 }
 
 var drawChart = function() {
-	// getStats();
-	// window.setTimeout(displayChart, 2000);
 
-	var d1 = getContacted(lists.contacted);
-	var d2 = getCalled(lists.called);
-	var d3 = getRejected(lists.rejected);
-	var d4 = getNotInterested(lists.notinterested);
-	var d5 = getInterviewed(lists.interviewed);
-	var d6 = getHired(lists.hired);
+	var d1 = getCardsFromList(lists.contacted);
+	var d2 = getCardsFromList(lists.called);
+	var d3 = getCardsFromList(lists.rejected);
+	var d4 = getCardsFromList(lists.notinterested);
+	var d5 = getCardsFromList(lists.interviewed);
+	var d6 = getCardsFromList(lists.hired);
+	var d7 = getCardsFromList(lists.hasResponse);
 
-	$.when(d1, d2, d3, d4, d5, d6).then(displayChart);
+	$.when(d1, d2, d3, d4, d5, d6, d7).then(refreshPage);
 }
 
-google.charts.load('current', {packages: ['corechart']});
+google.charts.load('current', {packages: ['bar']});
 google.charts.setOnLoadCallback(drawChart);
-
-// for(int i = 0 ; i < Object.keys(lists).length ; i++) {
-// 	var name = Object.keys(lists)[i];
-// 	var id = lists[i];
-// 	Trello.get('/lists/'+id+'/cards', success, fail);
-	
-// }
